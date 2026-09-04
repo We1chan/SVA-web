99ad8b70d070a676f712e6bead08e68bb4b1f0ee
ww1990442038 <1990442038@qq.com>
2026-09-04 18:47:29 +0800

    fix(web): restore license route component

---
+++ src/views/system/license/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <business-config />
+</template>
+
+<script>
+import BusinessConfig from '@/views/system/config/business.vue'
+
+export default {
+  name: 'SystemLicenseCompatibility',
+  components: {
+    BusinessConfig
+  }
+}
+</script>
